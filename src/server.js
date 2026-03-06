a96cb6abf322292b7ebd61f16d3dfebd6dd14856
nguyenDucThang15 <116487174+nguyenDucThang15@users.noreply.github.com>
2026-03-06 09:36:36 +0700

    fix CORS

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 import exitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from './config/mongodb.js'
 import { env } from './config/environment.js'
 import { APIs_V1 } from './routes/v1/index.js'
 import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware.js'
+import { corsOptions } from './config/cors.js'
 
 const START_SERVER = () => {
   const app = express()
+
+  app.use(cors(corsOptions))
 
   app.use(express.json())
 
